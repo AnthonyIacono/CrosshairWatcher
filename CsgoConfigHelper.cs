478e1c6bd3266402d32ffcde4da449499c62386d
aiacono <anthonyiacono@gmail.com>
2014-07-13 17:42:41 -0700

    Added newest crosshair variables.

--- CsgoConfigHelper.cs
+++ CsgoConfigHelper.cs
@@ -46,20 +46,24 @@ namespace CrosshairWatcher
 
             string fullCrosshairString = "";
 
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshairstyle", crosshairCommands, "0") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshairalpha", crosshairCommands, "200") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshaircolor", crosshairCommands, "5") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshaircolor", crosshairCommands, "1") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshaircolor_b", crosshairCommands, "50") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshaircolor_r", crosshairCommands, "50") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshaircolor_g", crosshairCommands, "250") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshairdot", crosshairCommands, "0") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshairgap", crosshairCommands, "0") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshairdot", crosshairCommands, "1") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshairgap", crosshairCommands, "1") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshairsize", crosshairCommands, "5") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshairstyle", crosshairCommands, "2") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshairusealpha", crosshairCommands, "1") + ";";
             fullCrosshairString += GetCrosshairVariableString("cl_crosshairthickness", crosshairCommands, "0.5") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_fixedcrosshairgap", crosshairCommands, "0") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_outline", crosshairCommands, "0") + ";";
-            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_outline_draw", crosshairCommands, "0") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_fixedcrosshairgap", crosshairCommands, "3") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_drawoutline", crosshairCommands, "1") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_outlinethickness", crosshairCommands, "1") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_dynamic_maxdist_splitratio", crosshairCommands, "0.35") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_dynamic_splitalpha_innermod", crosshairCommands, "1") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_dynamic_splitalpha_outermod", crosshairCommands, "0.5") + ";";
+            fullCrosshairString += GetCrosshairVariableString("cl_crosshair_dynamic_splitdist", crosshairCommands, "7") + ";";
 
             return fullCrosshairString;
         }
